33e2e16acdcade2477e1d5bb27ebfb65e39f8738
Paweł Kopiczko <kopiczko@gmail.com>
2017-05-19 13:33:06 +0200

    Use cenkalti/backoff

--- tpr/error.go
+++ tpr/error.go
@@ -10,3 +10,10 @@ var invalidConfigError = errgo.New("invalid config")
 func IsInvalidConfig(err error) bool {
 	return errgo.Cause(err) == invalidConfigError
 }
+
+var tprInitTimeoutError = errgo.New("initialization timeout")
+
+// IsTPRInitTimeout asserts tprInitTimeoutError.
+func IsTPRInitTimeout(err error) bool {
+	return errgo.Cause(err) == tprInitTimeoutError
+}
