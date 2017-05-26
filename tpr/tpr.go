dbaac1a28884a49088397502ff91dd19f302a711
Paweł Kopiczko <kopiczko@gmail.com>
2017-05-26 13:31:24 +0200

    Add *Endpoint methods

--- tpr/tpr.go
+++ tpr/tpr.go
@@ -1,7 +1,6 @@
 package tpr
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/cenkalti/backoff"
@@ -55,7 +54,9 @@ type TPR struct {
 	apiVersion  string // apiVersion is group/version
 	description string
 
-	endpoint string
+	// resourceName is name of a resource registered in Kubernetes API for
+	// this TPR kind name.
+	resourceName string
 }
 
 func New(config Config) (*TPR, error) {
@@ -77,8 +78,6 @@ func New(config Config) (*TPR, error) {
 		return nil, microerror.MaskAny(err)
 	}
 
-	resource := unsafeGuessKindToResource(kind)
-
 	tpr := &TPR{
 		clientset: config.Clientset,
 
@@ -89,7 +88,7 @@ func New(config Config) (*TPR, error) {
 		apiVersion:  group + "/" + config.Version,
 		description: config.Description,
 
-		endpoint: fmt.Sprintf("/apis/%s/%s/%s", group, config.Version, resource),
+		resourceName: unsafeGuessKindToResource(kind),
 	}
 	return tpr, nil
 }
@@ -108,6 +107,28 @@ func (t *TPR) Name() string { return t.name }
 
 // Group returns a TPR group extracted from Name. See Config.Name godoc for details.
 func (t *TPR) Group() string { return t.group }
+
+// Endpoint returns a TPR resurce endpoint registered in Kubernetes API under
+// a given namespace. Default namespace will be used when argument is an empty
+// string.
+func (t *TPR) Endpoint(namespace string) string {
+	nsResource := t.resourceName
+	if len(namespace) != 0 {
+		nsResource = "namespace/" + namespace + "/" + t.resourceName
+	}
+	return "/apis/" + t.group + "/" + t.version + "/" + nsResource
+}
+
+// Endpoint returns a TPR watch resurce endpoint registered in Kubernetes API
+// under a given namespace. Default namespace will be used when argument is an
+// empty string.
+func (t *TPR) WatchEndpoint(namespace string) string {
+	nsResource := "watch/" + t.resourceName
+	if len(namespace) != 0 {
+		nsResource = "namespace/" + namespace + "/watch/" + t.resourceName
+	}
+	return "/apis/" + t.group + "/" + t.version + "/" + nsResource
+}
 
 // CreateAndWait creates a TPR and waits till it is initialized in the cluster.
 func (t *TPR) CreateAndWait() error {
@@ -152,8 +173,9 @@ func (t *TPR) create() error {
 }
 
 func (t *TPR) waitInit(retry backoff.BackOff) error {
+	endpoint := t.Endpoint("")
 	op := func() error {
-		_, err := t.clientset.CoreV1().RESTClient().Get().RequestURI(t.endpoint).DoRaw()
+		_, err := t.clientset.CoreV1().RESTClient().Get().RequestURI(endpoint).DoRaw()
 		return err
 	}
 
