9bdd1adc8a3404bcd2afbadaa1be6d23d2cb3a1d
Paweł Kopiczko <kopiczko@gmail.com>
2017-05-30 11:22:03 +0200

    Rename unexpectedlyShortResourceNameError -> malformedNameError

--- tpr/error.go
+++ tpr/error.go
@@ -18,16 +18,16 @@ func IsInvalidConfig(err error) bool {
 	return errgo.Cause(err) == invalidConfigError
 }
 
+var malformedNameError = errgo.New("unexpectedly short resource name")
+
+// IsMalformedName asserts malformedNameError.
+func IsMalformedName(err error) bool {
+	return errgo.Cause(err) == malformedNameError
+}
+
 var tprInitTimeoutError = errgo.New("initialization timeout")
 
 // IsTPRInitTimeout asserts tprInitTimeoutError.
 func IsTPRInitTimeout(err error) bool {
 	return errgo.Cause(err) == tprInitTimeoutError
-}
-
-var unexpectedlyShortResourceNameError = errgo.New("unexpectedly short resource name")
-
-// IsUnexpectedlyShortResourceName asserts unexpectedlyShortResourceNameError.
-func IsUnexpectedlyShortResourceName(err error) bool {
-	return errgo.Cause(err) == unexpectedlyShortResourceNameError
 }
