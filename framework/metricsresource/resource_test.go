51d6e95f630f1127e0a9acb9be9e55f0a41f333b
Tim Schindler <xh3b4sd@gmail.com>
2017-08-15 15:22:16 +0200

    aligned metrics resource with new update primitives

--- framework/metricsresource/resource_test.go
+++ framework/metricsresource/resource_test.go
@@ -71,6 +71,40 @@ func Test_MetricsResource_ProcessDelete_ResourceOrder(t *testing.T) {
 	}
 }
 
+// Test_MetricsResource_ProcessUpdate_ResourceOrder ensures the resource's
+// methods are executed as expected when updating resources using the wrapping
+// prometheus resource.
+func Test_MetricsResource_ProcessUpdate_ResourceOrder(t *testing.T) {
+	tr := &testResource{}
+	rs := []framework.Resource{
+		tr,
+	}
+
+	config := DefaultWrapConfig()
+	config.Namespace = t.Name()
+	wrapped, err := Wrap(rs, config)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+
+	err = testMustNewFramework(t).ProcessUpdate(nil, nil, wrapped)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+
+	e := []string{
+		"GetCurrentState",
+		"GetDesiredState",
+		"GetUpdateState",
+		"ProcessCreateState",
+		"ProcessDeleteState",
+		"ProcessUpdateState",
+	}
+	if !reflect.DeepEqual(e, tr.Order) {
+		t.Fatal("expected", e, "got", tr.Order)
+	}
+}
+
 func testMustNewFramework(t *testing.T) *framework.Framework {
 	frameworkConfig := framework.DefaultConfig()
 	newFramework, err := framework.New(frameworkConfig)
@@ -113,6 +147,13 @@ func (r *testResource) GetDeleteState(obj, currentState, desiredState interface{
 	return nil, nil
 }
 
+func (r *testResource) GetUpdateState(obj, currentState, desiredState interface{}) (interface{}, interface{}, interface{}, error) {
+	m := "GetUpdateState"
+	r.Order = append(r.Order, m)
+
+	return nil, nil, nil, nil
+}
+
 func (r *testResource) Name() string {
 	return "testResource"
 }
@@ -126,6 +167,13 @@ func (r *testResource) ProcessCreateState(obj, createState interface{}) error {
 
 func (r *testResource) ProcessDeleteState(obj, deleteState interface{}) error {
 	m := "ProcessDeleteState"
+	r.Order = append(r.Order, m)
+
+	return nil
+}
+
+func (r *testResource) ProcessUpdateState(obj, updateState interface{}) error {
+	m := "ProcessUpdateState"
 	r.Order = append(r.Order, m)
 
 	return nil
