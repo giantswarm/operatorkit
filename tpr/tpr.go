cd857eadb2263e63e7d45d6eba0a9ea66f04f403
Oliver Ponder <oliver.ponder@gmail.com>
2017-10-25 15:22:03 +0200

    Use PermanentError as the right way to get out of a retry if the TPR already exists

--- tpr/tpr.go
+++ tpr/tpr.go
@@ -266,7 +266,7 @@ func (t *TPR) create(retry backoff.BackOff) error {
 	createTpr := func() error {
 		_, err := t.k8sClient.ExtensionsV1beta1().ThirdPartyResources().Create(tpr)
 		if err != nil && apierrors.IsAlreadyExists(err) {
-			return microerror.Mask(alreadyExistsError)
+			return backoff.Permanent(microerror.Mask(alreadyExistsError))
 		}
 		if err != nil {
 			return microerror.Maskf(err, "creating TPR %s", t.name)
@@ -275,20 +275,7 @@ func (t *TPR) create(retry backoff.BackOff) error {
 		return nil
 	}
 
-	// Try creating the TPR once without a retry, since I
-	// do not want to retry on the 'alreadyExistsError'.
-	err := createTpr()
-	if err != nil && apierrors.IsAlreadyExists(err) {
-		return microerror.Mask(alreadyExistsError)
-	}
-
-	// TPR created succesfully. Don't try to create it again with retries.
-	if err == nil {
-		return nil
-	}
-
-	// Now try creating the TPR with retries.
-	err = backoff.Retry(createTpr, retry)
+	err := backoff.Retry(createTpr, retry)
 	if err != nil {
 		return microerror.Mask(err)
 	}
