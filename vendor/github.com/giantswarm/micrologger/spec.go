436d4e3fbcbe0d57db9385133e97091eff1afab0
Tim Schindler <xh3b4sd@gmail.com>
2017-11-22 17:10:03 +0100

    updated vendor deps

--- vendor/github.com/giantswarm/micrologger/spec.go
+++ vendor/github.com/giantswarm/micrologger/spec.go
@@ -1,14 +1,19 @@
 package micrologger
 
+import "context"
+
 // Logger is a simple interface describing services that emit messages to gather
 // certain runtime information.
 type Logger interface {
 	// Log takes a sequence of alternating key/value pairs which are used to
 	// create the log message structure.
-	Log(v ...interface{}) error
-
-	// With returns a new contextual logger with keyvals appended to those
+	Log(keyVals ...interface{}) error
+	// LogWithCtx is the same as Log but additionally taking a context which may
+	// contain additional key-value pairs that are added to the log issuance, if
+	// any.
+	LogWithCtx(ctx context.Context, keyVals ...interface{}) error
+	// With returns a new contextual logger with keyVals appended to those
 	// passed to calls to Log. If logger is also a contextual logger
-	// created by With, keyvals is appended to the existing context.
-	With(keyvals ...interface{}) Logger
+	// created by With, keyVals is appended to the existing context.
+	With(keyVals ...interface{}) Logger
 }
