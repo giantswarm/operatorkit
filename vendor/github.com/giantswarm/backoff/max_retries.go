727977e0083fcdec32f034914cae820f4d46311f
Tim Schindler <xh3b4sd@gmail.com>
2018-08-07 22:38:05 +0200

    updates vendor deps

--- vendor/github.com/giantswarm/backoff/max_retries.go
+++ vendor/github.com/giantswarm/backoff/max_retries.go
@@ -1,9 +1,43 @@
 package backoff
 
-import "github.com/cenkalti/backoff"
+import (
+	"time"
 
-func WithMaxRetries(max uint64) Interface {
-	b := backoff.NewExponentialBackOff()
-	b.MaxElapsedTime = 0
-	return backoff.WithMaxRetries(b, max)
+	"github.com/cenkalti/backoff"
+)
+
+func NewMaxRetries(maxRetries uint64, maxInterval time.Duration) Interface {
+	b := withMaxRetries(backoff.NewConstantBackOff(maxInterval), maxRetries)
+
+	b.Reset()
+
+	return b
+}
+
+func withMaxRetries(b backoff.BackOff, m uint64) *backOffMaxRetries {
+	return &backOffMaxRetries{
+		maxRetries: m,
+		retryCount: 0,
+		underlying: b,
+	}
+}
+
+type backOffMaxRetries struct {
+	maxRetries uint64
+	retryCount uint64
+	underlying backoff.BackOff
+}
+
+func (b *backOffMaxRetries) NextBackOff() time.Duration {
+	if b.retryCount+1 >= b.maxRetries {
+		return backoff.Stop
+	}
+	b.retryCount++
+
+	return b.underlying.NextBackOff()
+}
+
+func (b *backOffMaxRetries) Reset() {
+	b.retryCount = 0
+	b.underlying.Reset()
 }
