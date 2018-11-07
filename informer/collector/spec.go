3996a69f7183fd6fe03b1ec2af3b519144578d87
Tim Schindler <xh3b4sd@gmail.com>
2018-11-07 22:14:09 +0100

    removes unused interface

--- informer/collector/spec.go
+++ informer/collector/spec.go
@@ -1,18 +1,9 @@
 package collector
 
 import (
-	"context"
-	"time"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 )
-
-type Interface interface {
-	Boot(ctx context.Context) error
-	ResyncPeriod() time.Duration
-	Watch(ctx context.Context) (chan watch.Event, chan watch.Event, chan error)
-}
 
 // Watcher provides Watch method compatible with Kubernetes clients.
 type Watcher interface {
