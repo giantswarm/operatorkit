64b5f5b313d2267b28b3394f469a1e64961c636d
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-20 16:16:20 +0000

    update to Kubernetes 1.12.2

--- vendor/github.com/giantswarm/apiextensions/pkg/clientset/versioned/clientset.go
+++ vendor/github.com/giantswarm/apiextensions/pkg/clientset/versioned/clientset.go
@@ -22,7 +22,6 @@ import (
 	corev1alpha1 "github.com/giantswarm/apiextensions/pkg/clientset/versioned/typed/core/v1alpha1"
 	examplev1alpha1 "github.com/giantswarm/apiextensions/pkg/clientset/versioned/typed/example/v1alpha1"
 	providerv1alpha1 "github.com/giantswarm/apiextensions/pkg/clientset/versioned/typed/provider/v1alpha1"
-	glog "github.com/golang/glog"
 	discovery "k8s.io/client-go/discovery"
 	rest "k8s.io/client-go/rest"
 	flowcontrol "k8s.io/client-go/util/flowcontrol"
@@ -114,7 +113,6 @@ func NewForConfig(c *rest.Config) (*Clientset, error) {
 
 	cs.DiscoveryClient, err = discovery.NewDiscoveryClientForConfig(&configShallowCopy)
 	if err != nil {
-		glog.Errorf("failed to create the DiscoveryClient: %v", err)
 		return nil, err
 	}
 	return &cs, nil
