71b3186d3357ed7cf7f106e7e46dcceb75be9ea8
Paweł Kopiczko <kopiczko@gmail.com>
2019-02-28 14:53:17 +0000

    [manual] update StateGetter godoc

--- resource/configmap/spec.go
+++ resource/configmap/spec.go
@@ -8,10 +8,14 @@ import (
 
 type StateGetter interface {
 	// GetCurrentState returns a current state of the system for the given
-	// incarnation of the observed Kubernetes object.
+	// incarnation of the observed Kubernetes object. The state consists of
+	// multiple objects but it is fine to return a slice of single object
+	// if only one object is managed by this resource.
 	GetCurrentState(ctx context.Context, obj interface{}) ([]*corev1.ConfigMap, error)
 	// GetDesiredState returns a desired state of the system for the given
-	// carnation of the observed Kubernetes object.
+	// incarnation of the observed Kubernetes object. The state consists of
+	// multiple objects but it is fine to return a slice of single object
+	// if only one object is managed by this resource.
 	//
 	// NOTE: This state may be different if the observed object is
 	// created/updated or deleted. Deletion timestamp can be checked to
