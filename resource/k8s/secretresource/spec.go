785755f085035d76837def3294ecaf73ae8af241
Tim Schindler <xh3b4sd@gmail.com>
2019-10-30 18:25:23 +0100

    use proper resource package

--- resource/k8s/secretresource/spec.go
+++ resource/k8s/secretresource/spec.go
@@ -3,18 +3,18 @@ package secretresource
 import (
 	"context"
 
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type StateGetter interface {
 	// GetCurrentState returns a current state of the system for the given
 	// carnation of the observed Kubernetes object.
-	GetCurrentState(ctx context.Context, obj interface{}) ([]*v1.Secret, error)
+	GetCurrentState(ctx context.Context, obj interface{}) ([]*corev1.Secret, error)
 	// GetDesiredState returns a desired state of the system for the given
 	// carnation of the observed Kubernetes object.
 	//
 	// NOTE: This state may be different if the observed object is
 	// created/updated or deleted. Deletion timestamp can be checked to
 	// figure if the object is scheduled for deletion.
-	GetDesiredState(ctx context.Context, obj interface{}) ([]*v1.Secret, error)
+	GetDesiredState(ctx context.Context, obj interface{}) ([]*corev1.Secret, error)
 }
