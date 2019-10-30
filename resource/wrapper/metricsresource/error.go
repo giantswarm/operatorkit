be88e406dded8d7ad8052372f0cb07d006e90523
Tim Schindler <tim@giantswarm.io>
2019-10-30 18:16:40 +0100

    align metricsresource with retryresource (#331)

--- resource/wrapper/metricsresource/error.go
+++ resource/wrapper/metricsresource/error.go
@@ -4,15 +4,6 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var incompatibleUnderlyingResourceError = &microerror.Error{
-	Kind: "incompatibleUnderlyingResourceError",
-}
-
-// isIncompatibleUnderlyingResource asserts incompatibleUnderlyingResourceError.
-func isIncompatibleUnderlyingResource(err error) bool {
-	return microerror.Cause(err) == incompatibleUnderlyingResourceError
-}
-
 var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
