23deaab1fd93d37dcb85ad5a06df5f8f954e4595
Nick Jüttner <nick@giantswarm.io>
2020-06-24 15:35:34 +0200

    event recorder

--- controller/internal/recorder/recorder.go
+++ controller/internal/recorder/recorder.go
@@ -3,8 +3,6 @@ package recorder
 import (
 	"github.com/giantswarm/k8sclient/v3/pkg/k8sclient"
 	clientv1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/kubernetes/fake"
-	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/tools/record"
 )
 
@@ -16,8 +14,5 @@ type Config struct {
 // New creates an event recorder to send custom events to Kubernetes to be recorded for targeted Kubernetes objects
 func New(c Config) record.EventRecorder {
 	eventBroadcaster := record.NewBroadcaster()
-	if _, isfake := c.K8sClient.K8sClient().(*fake.Clientset); !isfake {
-		eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: v1core.New(c.K8sClient.K8sClient().CoreV1().RESTClient()).Events("")})
-	}
 	return eventBroadcaster.NewRecorder(c.K8sClient.Scheme(), clientv1.EventSource{Component: c.Component})
 }
