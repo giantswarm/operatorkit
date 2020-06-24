e23c17caa31db26f92a090de8d5070cf1f2ec6dd
Nick Jüttner <nick@giantswarm.io>
2020-06-24 14:45:38 +0200

    event recorder

--- controller/internal/recorder/recorder.go
+++ controller/internal/recorder/recorder.go
@@ -10,14 +10,14 @@ import (
 
 type Config struct {
 	Component string
-	K8sclient k8sclient.Interface
+	K8sClient k8sclient.Interface
 }
 
 // New creates an event recorder to send custom events to Kubernetes to be recorded for targeted Kubernetes objects
 func New(c Config) record.EventRecorder {
 	eventBroadcaster := record.NewBroadcaster()
-	if _, isfake := c.K8sclient.K8sClient().(*fake.Clientset); !isfake {
-		eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: v1core.New(c.K8sclient.K8sClient().CoreV1().RESTClient()).Events("")})
+	if _, isfake := c.K8sClient.K8sClient().(*fake.Clientset); !isfake {
+		eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: v1core.New(c.K8sClient.K8sClient().CoreV1().RESTClient()).Events("")})
 	}
-	return eventBroadcaster.NewRecorder(c.K8sclient.Scheme(), clientv1.EventSource{Component: c.Component})
+	return eventBroadcaster.NewRecorder(c.K8sClient.Scheme(), clientv1.EventSource{Component: c.Component})
 }
