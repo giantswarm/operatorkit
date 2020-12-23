4545cb4666171ab4f0c6226b9597919f6f93e2ea
Christian Bianchi <whites11@gmail.com>
2020-12-23 10:38:30 +0100

    Added troubleshooting code

--- pkg/controller/internal/sentry/default.go
+++ pkg/controller/internal/sentry/default.go
@@ -4,13 +4,11 @@ import (
 	"context"
 
 	"github.com/getsentry/sentry-go"
-	"github.com/giantswarm/microerror"
 )
 
 type Default struct {
 }
 
 func (s *Default) Capture(ctx context.Context, err error) {
-	sentry.CaptureException(microerror.Mask(err))
 	sentry.CaptureException(err)
 }
