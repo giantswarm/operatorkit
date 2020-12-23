653979a1d3541f3ee28f1ad43a1b514cbbda6051
Christian Bianchi <whites11@gmail.com>
2020-12-23 10:52:07 +0100

    Added troubleshooting code

--- pkg/controller/internal/sentry/default.go
+++ pkg/controller/internal/sentry/default.go
@@ -12,6 +12,37 @@ type Default struct {
 }
 
 func (s *Default) Capture(ctx context.Context, err error) {
-	fmt.Printf("type: %s\n", reflect.TypeOf(err).String())
+	method := extractReflectedStacktraceMethod(err)
+	fmt.Printf("valid: %t, name: %s\n", method.IsValid(), method.String())
 	sentry.CaptureException(err)
+}
+
+func extractReflectedStacktraceMethod(err error) reflect.Value {
+	var method reflect.Value
+
+	// https://github.com/pingcap/errors
+	methodGetStackTracer := reflect.ValueOf(err).MethodByName("GetStackTracer")
+	// https://github.com/pkg/errors
+	methodStackTrace := reflect.ValueOf(err).MethodByName("StackTrace")
+	// https://github.com/go-errors/errors
+	methodStackFrames := reflect.ValueOf(err).MethodByName("StackFrames")
+
+	if methodGetStackTracer.IsValid() {
+		stacktracer := methodGetStackTracer.Call(make([]reflect.Value, 0))[0]
+		stacktracerStackTrace := reflect.ValueOf(stacktracer).MethodByName("StackTrace")
+
+		if stacktracerStackTrace.IsValid() {
+			method = stacktracerStackTrace
+		}
+	}
+
+	if methodStackTrace.IsValid() {
+		method = methodStackTrace
+	}
+
+	if methodStackFrames.IsValid() {
+		method = methodStackFrames
+	}
+
+	return method
 }
