ed6d3d909bbf52b8a23cc802c953eeae3420e284
Christian Bianchi <whites11@gmail.com>
2020-12-23 10:43:23 +0100

    Added troubleshooting code

--- pkg/controller/internal/sentry/default.go
+++ pkg/controller/internal/sentry/default.go
@@ -2,6 +2,8 @@ package sentry
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/getsentry/sentry-go"
 )
@@ -10,5 +12,9 @@ type Default struct {
 }
 
 func (s *Default) Capture(ctx context.Context, err error) {
+	fmt.Println("=========================")
+	j, _ := json.Marshal(err)
+	fmt.Println(string(j))
+	fmt.Println("=========================")
 	sentry.CaptureException(err)
 }
