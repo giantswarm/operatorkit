962d6d6f683e06b67564045f30afae982b64f131
pipo02mix <fernando@giantswarm.io>
2025-01-27 12:54:49 +0100

    Add logging to know more about the error

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -22,6 +22,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -243,6 +244,8 @@ func New(config Config) (*Controller, error) {
 		namespace:    config.Namespace,
 		resyncPeriod: config.ResyncPeriod,
 	}
+	ctx := context.Background()
+	c.logger.Debugf(ctx, "Controller config is %v", config)
 
 	return c, nil
 }
@@ -351,6 +354,27 @@ func (c *Controller) bootWithError(ctx context.Context) error {
 			reconcileErrors.WithLabelValues(c.name).Set(0)
 		}
 	}()
+
+	// We overwrite the k8s error handlers so they do not intercept our log
+	// streams. The format is way easier to parse for us that way. Here we also
+	// emit metrics for the occurred errors to ensure we create more awareness of
+	// anything going wrong in our operators.
+	{
+		utilruntime.ErrorHandlers = []utilruntime.ErrorHandler{
+			func(_ context.Context, err error, msg string, _ ...interface{}) {
+				// When we see a port forwarding error we ignore it because we cannot do
+				// anything about it. Errors like we check here would have to be dealt
+				// with in the third party tools we use. The port forwarding in general
+				// is broken by design which will go away with Helm 3, soon TM.
+				if IsPortforward(err) {
+					return
+				}
+
+				reconcileErrors.WithLabelValues(c.name).Inc()
+				c.logger.Errorf(ctx, err, msg+" caught third party runtime error")
+			},
+		}
+	}
 
 	var mgr manager.Manager
 	{
